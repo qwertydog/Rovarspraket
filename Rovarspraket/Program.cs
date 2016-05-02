b1f86c0e4fa1e27b6d5abff8192c328a4ee727bd
qwertydog <campbelldeas@hotmail.com>
2016-05-02 14:24:22 +1200

    Completed solution

--- Rovarspraket/Program.cs
+++ Rovarspraket/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Rovarspraket
 {
@@ -10,9 +7,51 @@ namespace Rovarspraket
     {
         static void Main(string[] args)
         {
+            while (true)
+            {
+                string output = EncodeRovarspraket(Console.ReadLine());
+                Console.WriteLine(output);
 
+                output = DecodeRovarspraket(output);
+                Console.WriteLine(output);
+            }
         }
 
+        private static string EncodeRovarspraket(string input)
+        {
+            string vowels = "AEIOUYÅÄÖ",
+                   output = string.Empty;
 
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (char.IsLetter(input[i]) && !vowels.Contains(char.ToUpper(input[i])))
+                {
+                    string token = input[i] + "o" + char.ToLower(input[i]);
+                    output += token;
+                }
+                else
+                {
+                    output += input[i];
+                }
+            }
+            return output;
+        }
+
+        private static string DecodeRovarspraket(string input)
+        {
+            string vowels = "AEIOUYÅÄÖ",
+                   output = string.Empty;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                output += input[i];
+
+                if (char.IsLetter(input[i]) && !vowels.Contains(char.ToUpper(input[i])))
+                {
+                    i += 2;
+                }
+            }
+            return output;
+        }
     }
 }
